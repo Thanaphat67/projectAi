55867fac2bec0c0e91668be102d4f486707bc400
Thanaphat <thanaphat.jamroen@gmail.com>
2025-02-09 09:56:52 +0700

    ตั้งต้นโปรเจค

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb")
+const app = express();
+
+let db;
+const client = new MongoClient("mongodb://localhost:27017");
+client.connect().then(() => {
+    db = client.db("ecommerce");
+    console.log("MongoDB connected")
+}).catch(() => {
+    console.log("MongoDB unconnect")
+});
+
+
+// ดึงข้อมูลทั้งหมด
+app.get('/product', async (req, res) => {
+    const product = await db.collection("product").find().toArray();
+    res.json(product);
+});
+
+// ดึงข้อมูลรายการนั้น
+app.get('/product/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await db.collection("product").findOne({
+            "_id": new ObjectId(id)
+        });
+        res.json(product);
+    } catch (err) {
+        res.json("error");
+    }
+});
+
+app.post('/product', async (req, res) => {
+    try {
+        const data = req.body;
+        const product = await db.collection("product").inserto(data);
+        res.json(product);
+    }
+    catch (err) {
+        res.json("error");
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server started: success');
+});
